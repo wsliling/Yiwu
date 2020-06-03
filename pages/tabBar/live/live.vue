d9a4a013628f16533501e8fc0328bba77b528609
liling <907247899@qq.com>
2020-06-03 14:47:02 +0800

    商城视频页面

--- pages/tabBar/live/live.vue
+++ pages/tabBar/live/live.vue
@@ -1,24 +1,152 @@
 <template>
-	<view>
+	<view class="content">
+		<view class="head">
+			<view class="index_head flex-between">
+				<view class="head_l">
+					<image src="@/static/live.png" class="iconimg"></image>
+				</view>
+				<view class="title">视频</view>
+				<view class="head_r">
+					<image src="@/static/video.png" class="iconimg"></image>
+				</view>
+			</view>
+			<scroll-view id="tab-bar" class="index-swiper-tab" scroll-x :scroll-left="scrollLeft">
+				<view v-for="(tab,index) in tabnav" :key="index" :class="['item',tabIndex==index ? 'active' : '']" :id="'tabNum'+index" :data-current="index" @click="tapTab(index,tab.Id)">{{tab.TypeName}}</view>
+				<view class="bb_line" :style="'left:'+tabStyle+'rpx'"></view>
+			</scroll-view>
+		</view>
+		<view style="height: 80px;"></view>
+		<view class="search">
+			<view class="seachbox">
+				<text class="uni-icon uni-icon-search">请输入搜索关键字</text>
+			</view>
+		</view>
+		<view class="videolist">
+			<view class="Yi-media" v-for="(item,index) in 5" :key="index">
+				<view class="media-bd">
+					<view class="maxpic p_re">
+						<image src="@/static/of/p1.jpg" mode="widthFix"></image>
+						<view class="isplay"></view>
+						<view class="desc">
+							Michael Malitowski &Joanna Leunis，Rumba WSSDF 2016
+						</view>
+					</view>
+					<view class="media-ft flex-between">
+						<view class="ft_l flex-start">
+							<view class="author flex-start">
+								<view class="tx">
+									<image src="@/static/default.png" mode="aspectFill"></image>
+									<view class="islive">
+										<view class="line line1"></view>
+										<view class="line line2"></view>
+										<view class="line line3"></view>
+										<view class="txt">直播</view>
+									</view>
+								</view>
+								<view class="name uni-ellipsis">kastyle</view>
+							</view>
+						</view>
+						<view class="ft_r flex-end">
+							<view class="txt_info like active">124</view>
+							<view class="txt_info reply">678</view>
+							<view class="txt_info share"></view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
 		
+		<!-- #ifndef MP-WEIXIN -->
+		<view style="height: 100upx;"></view>
+		<!-- #endif -->
 	</view>
 </template>
 
 <script>
+	import {post,get,toLogin} from '@/common/util.js';
 	export default {
 		components: {
 		},
 		data() {
 			return {
-				
+				userId: "",
+				token: "",
+				scrollLeft: 0,
+				tabIndex:0,
+				tabnav:[
+					{
+						Id:1,
+						TypeName:"推荐"
+					},
+					{
+						Id:2,
+						TypeName:"最新"
+					},
+					{
+						Id:3,
+						TypeName:"教学"
+					},
+					{
+						Id:4,
+						TypeName:"比赛"
+					},
+					{
+						Id:5,
+						TypeName:"练习"
+					},
+					{
+						Id:6,
+						TypeName:"全部"
+					}
+				]
 			}
 		},
+		onLoad() {
+			this.userId = uni.getStorageSync("userId");
+			this.token = uni.getStorageSync("token");
+			
+		},
+		onShow(){
+			
+		},
+		computed: {
+		   tabStyle(){
+		     return ((750/this.tabnav.length)*this.tabIndex)+(((750/this.tabnav.length)-68)/2)
+		   }
+		 },
 		methods: {
+			//跳转
+			tolink(Url,islogin) {
+				if(islogin=="login"){
+					if(toLogin()){
+						uni.navigateTo({
+							url: Url
+						})
+					}
+				}else{
+					uni.navigateTo({
+						url: Url
+					})
+				}
+			},
+			tapTab(index,id) { //点击tab-bar
+				if (this.tabIndex === index) {
+					return false;
+				} else {
+					this.tabIndex = index;
+				}
+			},
 			
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	@import './style';
+	page{
+		background: #fff;
+	}
+	.index-swiper-tab .item{
+		width: 16.66%;
+	}
 </style>
--- pages/tabBar/shop/shop.vue
+++ pages/tabBar/shop/shop.vue
@@ -1,12 +1,121 @@
 <template>
 	<view>
 		<view class="search">
-			<view class="searchbox flex-start">
-				<view class="iconfont icon-sousuo"></view>
-				<view class="txt">搜索目的地/景点/星语</view>
+			<view class="seachbox">
+				<text class="uni-icon uni-icon-search">请输入搜索关键字</text>
+			</view>
+		</view>
+		<!--轮播图-->
+		<view class="shop_swiper pd15">
+			<swiper class="swiper" :indicator-dots="false" autoplay :interval="5000" :duration="500" @change="changeSwiper">
+				<swiper-item v-for="(item,index) in 3" :key="index">
+					<view class="swiper-item">
+						<image class="img" src="@/static/of/banner.jpg" mode="aspectFill"></image>
+					</view>
+				</swiper-item>
+			</swiper>
+			<view class="dots">
+				<view v-for="(item,index) in 3" :key="index" :class="['dot',currentSwiper==index?'active':'']"></view>
+			</view>
+		</view>
+		<!-- 快捷导航 -->
+		<view class="icon-menu">
+			<view class="item">
+				<image class="icon" src="@/static/fenlei1.png"></image>
+				<text class="txt">舞衣</text>
+			</view>
+			<view class="item">
+				<image class="icon" src="@/static/fenlei2.png"></image>
+				<text class="txt">舞裙</text>
+			</view>
+			<view class="item">
+				<image class="icon" src="@/static/fenlei3.png"></image>
+				<text class="txt">舞裤</text>
+			</view>
+			<view class="item">
+				<image class="icon" src="@/static/fenlei4.png"></image>
+				<text class="txt">舞鞋</text>
+			</view>
+			<view class="item">
+				<image class="icon" src="@/static/fenlei5.png"></image>
+				<text class="txt">全部</text>
+			</view>
+		</view>
+		<!-- 定制 -->
+		<image class="gg_dz" src="@/static/gg_1.png" mode="widthFix"></image>
+		<!-- 营销 -->
+		<view class="Yi-marketing pd15 flex">
+			<view class="item flex-between">
+				<view class="item_l">
+					<view class="txt_1">畅销榜</view>
+					<view class="txt_2">值得购买</view>
+				</view>
+				<view class="item_r">
+					<image src="@/static/z1.png"></image>
+				</view>
+			</view>
+			<view class="item flex-between">
+				<view class="item_l">
+					<view class="txt_1">每日上新</view>
+					<view class="txt_2">日日上新</view>
+				</view>
+				<view class="item_r">
+					<image src="@/static/z2.png"></image>
+				</view>
+			</view>
+		</view>
+		<!-- 每日分享 -->
+		<view class="Yi-Day uni-mb10">
+			<view class="Yi-hd flex-between">
+				<view class="title">每日分享</view>
+			</view>
+			<view class="Yi-bd pd15">
+				<scroll-view class="scroll-view_H Hotsell-list" scroll-x="true" scroll-left="0">
+					<view class="scroll-view-item_H" v-for="(item,index) in 8" :key="index">
+						<view class="itembox">
+							<view class="image-view">
+								<image class="img" src="@/static/of/pro1.jpg" mode="aspectFill"></image>
+							</view>
+							<view class="txtbox">
+								<view class="uni-product-price">
+									<text class="color_red">￥39</text>
+									<text class="uni-product-price-favour">￥159</text>
+								</view>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		<!-- 商品列表 -->
+		<view class="goods">
+			<view class="menu_nav flex-between">
+				<view class="menu_item flex flexAlignCenter flexColumn" 
+				v-for="(item,index) in navlist" :key="index" :class="{'active':index==indexs}" @click="tapTab(index)">
+					<view class="title">{{item.title}}</view>
+					<view class="subtitle">{{item.desc}}</view>
+				</view>
+			</view>
+			<view class="list flexWrap flex-between">
+				<view class="item" v-for="(item,index) in 6" :key="index">
+					<image src="@/static/of/pro1.jpg" class="item_img" mode="aspectFill"></image>
+					<view class="item_info">
+						<view class="item_title uni-ellipsis">一舞拉丁黑色舞蹈现身材连衣裙</view>
+						<view class="flexAlignEnd flex-between item_total">
+							<view class="flex flexAlignEnd">
+								<span class="item_price">￥79</span>
+								<span class="item_market line-through">￥88</span>
+							</view>
+							<view class="item_market">55人付款</view>
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 		
+		<!-- #ifndef MP-WEIXIN -->
+		<view style="height: 100upx;"></view>
+		<!-- #endif -->
 	</view>
 </template>
 
@@ -17,15 +126,35 @@
 		},
 		data() {
 			return {
-				
+				userId: "",
+				token: "",
+				currentSwiper :0,
+				navlist:[{id:1,title:'热卖推荐',desc:"本周销量TOP"},{id:2,title:'猜你喜欢',desc:"最懂你的"},{id:3,title:'便宜好货',desc:"好货不贵"}],
+				indexs:0,
 			}
 		},
-		methods: {
+		onLoad() {
 			
+		},
+		onShow() {
+			this.userId = uni.getStorageSync("userId");
+			this.token = uni.getStorageSync("token");
+		},
+		methods: {
+			changeSwiper(e){
+				this.currentSwiper=e.detail.current;
+			},
+			//切换
+			tapTab(index) {
+				this.indexs = index;
+			},
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	@import './style';
+	page{
+		background: #fff;
+	}
 </style>
