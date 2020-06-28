c5e1c82a4f16cc6fc1a4f61cf574563feaaf3f52
liling <907247899@qq.com>
2020-06-28 19:15:56 +0800

    00

--- pages/member/myBuy/myBuy.vue
+++ pages/member/myBuy/myBuy.vue
@@ -1,31 +1,20 @@
 <template>
 	<view>
-		<view class="head" :style="{'padding-top':barHeight+'px'}">
-			<view class="tab_head flex-between">
-				<!-- #ifndef MP-WEIXIN -->
-				<view class="head_l" @click="toback"><text class="uni-icon uni-icon-arrowleft"></text></view>
-				<view class="mine">我购买的</view>
-				<!-- #endif -->
-				<!-- #ifdef MP-WEIXIN -->
-				<view></view>
-				<view></view>
-				<!-- #endif --> 
-				<view class="redact" @click="ShowDel">{{isShowDel?'完成':'管理'}}</view>
-			</view>
-			<scroll-view id="tab-bar" class="index-swiper-tab" scroll-x>
-				<view style="width: 50%;" v-for="(tab,index) in tabnav" :key="index" :class="['item',tabIndex==index ? 'active' : '']" :id="'tabNum'+index" :data-current="index" @click="tapTab(index,tab.Id)">{{tab.TypeName}}</view>
-				<view class="bb_line" :style="'left:'+tabStyle+'rpx'"></view>
-			</scroll-view>
-		</view> 
-		<view :style="{height:(84+barHeight)+'px'}"></view>
-		
+		<scroll-view id="tab-bar" class="index-swiper-tab" scroll-x>
+			<view style="width: 50%;" v-for="(tab,index) in tabnav" :key="index" :class="['item',tabIndex==index ? 'active' : '']" :id="'tabNum'+index" :data-current="index" @click="tapTab(index)">{{tab.TypeName}}</view>
+			<view class="bb_line" :style="'left:'+tabStyle+'rpx'"></view>
+		</scroll-view>
+		<view style="height:40px"></view>
 		<view class="bb_pt uni-bg-white" v-if="hasData">
 			<view :class="['listbox',tabIndex==1?'musicbox':'']" v-for="(val, index) in datalist" :key="index">
 				<view class="choose" v-if="isShowDel" @click.stop="shiftChecked(index)"><view class="IconsCK IconsCK-radio" :class="{ checked: val.checked }"></view></view>
 				<view class="drawing flex">
-					<view class=""><image class="imgs" :src="val.PicFrist" mode=""></image></view>
+					<view class=""><image class="imgs" v-if="tabIndex==0" :src="val.PicImg" mode=""></image><image class="imgs" v-if="tabIndex==1" :src="val.PicImg||'/static/default_music.png'" mode=""></image></view>
 					<view class="brace">
-						<view :class="['being',tabIndex==1?'uni-ellipsis':'uni-ellipsis2']">{{ val.ProName }}</view>
+						<view :class="['being',tabIndex==1?'uni-ellipsis':'uni-ellipsis2']">{{ val.Name }}</view>
+						<view class="fz12 c_999" v-if="tabIndex==0">
+							{{val.SalesNum}}人购买
+						</view>
 						<view class="correct">
 							<span class="spanl">¥</span>{{ val.Price }}
 						</view>
@@ -52,8 +41,6 @@ import noData from '@/components/noData.vue'; //暂无数据
 export default {
 	data() {
 		return {
-			barHeight:0,
-			isShowDel: false, //编辑完成
 			userId: '',
 			token: '',
 			tabIndex:0,
@@ -68,15 +55,12 @@ export default {
 				}
 			],
 			page:1,
-			pageSize:4,
+			pageSize:5,
 			loadingType: 0, //0加载前，1加载中，2没有更多了
 			isLoad: false,
 			hasData: false,
 			noDataIsShow: false,
 			datalist: {}, //列表
-			datalength: 0,
-			Ids: [], //保存要删除数据
-			checked: false
 		};
 	},
 	components: {
@@ -89,13 +73,6 @@ export default {
 	  }
 	 },
 	onLoad() {
-		// #ifdef APP-PLUS
-		var height = plus.navigator.getStatusbarHeight();
-		this.barHeight = height;
-		// #endif
-		// #ifdef H5
-		this.barHeight = 0;
-		// #endif
 		this.userId = uni.getStorageSync('userId');
 		this.token = uni.getStorageSync('token');
 		this.workeslist();
@@ -107,89 +84,32 @@ export default {
 				url: Url
 			})
 		},
-		toback() {
-			uni.switchTab({
-				url: '/pages/tabBar/my/my'
-			});
-		},
-		//点击编辑 完成
-		ShowDel() {
-			this.isShowDel = !this.isShowDel;
-		},
 		 //点击tab-bar
-		tapTab(index,id) {
+		tapTab(index) {
 			if (this.tabIndex === index) {
 				return false;
 			} else {
 				this.tabIndex = index;
-			}
-		},
-		// 全选
-		cancelDel() {
-			this.checked = !this.checked;
-			if (this.checked) {
-				this.datalist.forEach(function(item) {
-					item.checked = true;
-				});
-			} else {
-				this.datalist.forEach(function(item) {
-					item.checked = false;
-				});
-			}
-		},
-		//选择
-		shiftChecked(key) {
-			console.log(key, 'key');
-			this.datalist[key].checked = !this.datalist[key].checked;
-			let _this = this;
-			let sum = 0;
-			_this.datalist.forEach(function(item) {
-				if (item.checked !== _this.checked) {
-					sum += 1;
-				}
-			});
-			if (sum == this.datalist.length) {
-				_this.checked = this.datalist[0].checked;
-			} else {
-				_this.checked = false;
+				this.page=1;
+				this.hasData=false;
+				this.noDataIsShow=false;
+				this.workeslist();
 			}
 		},
 		/*获取列表*/
 		async workeslist() {
-			// let result = await post('User/Memberdatalist', {
-			// 	UserId: this.userId,
-			// 	Token: this.token,
-			// 	page: this.page,
-			// 	pageSize: this.pageSize
-			// });
-			let result={
-				code:0,
-				data:[
-					{
-						Id:1,
-						Price:16.8,
-						ProName:"拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞",
-						PicFrist:'/static/of/pro1.jpg',
-						zan:"268人点赞"
-					},
-					{
-						Id:2,
-						Price:0,
-						ProName:"拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉",
-						PicFrist:'/static/of/pro1.jpg',
-						zan:"268人点赞"
-					},
-					{
-						Id:3,
-						Price:16.8,
-						ProName:"拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞拉丁舞",
-						PicFrist:'/static/of/pro1.jpg',
-						zan:"268人点赞"
-					}
-					
-				]
+			let url="";
+			if(this.tabIndex==0){
+				url="Course/MyCourseBuyList"
+			}else if(this.tabIndex==1){
+				url="DanceMusic/MyDanceMusicBuyList"
 			}
-			
+			let result = await post(url, {
+				UserId: this.userId,
+				Token: this.token,
+				page: this.page,
+				pageSize: this.pageSize
+			});
 			if (result.code === 0) {
 				let _this=this;
 				if (result.data.length > 0) {
@@ -222,105 +142,32 @@ export default {
 				this.datalength = this.datalist.length;
 			}
 		},
-		// 删除
-		btnDel() {
-			let _this = this;
-			this.datalist.forEach(function(item) {
-				if (item.checked == true) {
-					_this.Ids.push(item.Id);
-				}
-				console.log(_this.Ids, '_this.Ids');
-			});
-			if (this.Ids.length > 0) {
-				uni.showModal({
-				  content: "您确定要删除所选项吗？",
-				  confirmColor:"#df2271",
-				  success: function(res) {
-					if (res.confirm) {
-						  _this.DeleteMyFootprint();
-						} else if (res.cancel) {
-						
-						}
-					}
-				}) 
-				
-			} else {
-				uni.showToast({
-					title: '请选择需要删除的项！',
-					icon: 'none',
-					duration: 1500
-				});
-			}
-		},
-		async DeleteMyFootprint() {
-			let result = await post('User/DeleteMyFootprint', {
-				UserId: this.userId,
-				Token: this.token,
-				Id: this.Ids.join(',')
-			});
-			if (result.code === 0) {
-				let _this = this;
-				for (let j = 0; j < _this.Ids.length; j++) {
-					for (let i = 0; i < _this.datalength; i++) {
-						if (_this.datalist[i]) {
-							if (_this.Ids[j] == _this.datalist[i].Id) {
-								_this.datalist.splice(i, 1);
-							}
-						}
-					}
-				}
-				_this.datalength = _this.datalist.length;
-				_this.Ids = [];
-				uni.showToast({
-					title: result.msg,
-					icon: 'none',
-					duration: 2000
-				});
-			} else {
-				uni.showToast({
-					title: result.msg,
-					icon: 'none',
-					duration: 2000
-				});
-			}
-		},
-		// 上拉加载
-		onReachBottom: function() {
-			if (this.isLoad) {
-				this.loadingType = 1;
-				this.page++;
-				this.workeslist();
-			} else {
-				this.loadingType = 2;
-			}
+		
+	},
+	// 上拉加载
+	onReachBottom: function() {
+		if (this.isLoad) {
+			this.loadingType = 1;
+			this.page++;
+			this.workeslist();
+		} else {
+			this.loadingType = 2;
 		}
 	}
 };
 </script>
 
 <style scoped lang="scss">	
-.head {
-	.tab_head{
-		height: 44px;
-	}
-	.head_l{
-		.uni-icon{ font-size: 26px; margin: 0 5px;}
-	}
-	.mine {
-		font-size: 16px;
-		font-family: PingFang;
-		font-weight: bold;
-		color: rgba(51, 51, 51, 1);
-	}
-	.redact {
-		font-size: 14px;
-		font-family: PingFang;
-		font-weight: 500;
-		color: #999;
-		padding-right: 15px;
-	}
+.index-swiper-tab{
+	position: fixed;
+	left: 0;
+	top: 0;
+	/* #ifdef H5 */
+	top: 44px;
+	/* #endif */
+	width: 100%;
+	z-index: 9;
 }
-
 .listbox {
 	padding: 30upx;
 	display: flex;
---
+++ pages/message/messageClass/messageClass.vue
@@ -0,0 +1,107 @@
+<template>
+	<view>
+		<view class="uni-list" v-if="hasData">
+			<block v-for="(item,index) in TypeList" :key="index">
+				<view class="uni-list-cell" @click="tolink('/pages/message/messageList/messageList?type='+item.MsgType+'&keyname='+item.MsgName+'&shopId='+item.ShopId)">
+					<view class="uni-media-list">
+						<view class="uni-media-list-logo">
+							<image class="img" :src="item.TypeImg" mode="aspectFill"></image>
+						</view>
+						<view  class="uni-media-list-body">
+							<view class="uni-media-list-text-top uni-ellipsis">{{item.MsgName}}</view>
+							<view  class="uni-media-list-text-bottom uni-ellipsis">
+								{{item.NoticeTitle}}
+							</view>
+						</view>
+						<view class="uni-media-list-r">
+							<view class="time">{{item.PubTime}}</view>
+							<view class="uni-badge uni-badge-red" v-if="item.unCount!=0">{{item.unCount}}</view>
+						</view>
+					</view>
+				</view>
+			</block>
+		</view>
+		<noData :isShow="noDataIsShow"></noData>
+	</view>
+</template>
+
+<script>
+	import {host,post,get,dateUtils,toLogin} from '@/common/util.js';
+	import noData from '@/components/noData.vue'; //暂无数据
+	export default {
+		components: {
+			noData
+		},
+		data() {
+			return {
+				userId: "",
+				token: "",
+				hasData:false,
+				noDataIsShow: false,
+				TypeList:[]
+			};
+		},
+		onLoad() {
+			this.userId = uni.getStorageSync("userId");
+			this.token = uni.getStorageSync("token");
+		},
+		onShow() {
+			this.NoticeTypeList();
+		},
+		methods:{
+			async NoticeTypeList() {
+				let result = await post("News/NoticeTypeList", {
+					"UserId": this.userId,
+					"Token": this.token
+				});
+				if (result.code === 0) {
+					console.log(result,"news")
+					this.TypeList=result.data;
+					if (result.data.length > 0){
+						this.hasData = true;
+						result.data.forEach(function(item){
+							item.PubTime=dateUtils.format(item.PubTime);
+						})
+					}else{
+						this.noDataIsShow = true;
+					}
+				} else if (result.code === 2) {
+					let _this = this;
+					uni.showToast({
+						title: "登录超时，请重新登录!",
+						icon: "none",
+						duration: 2000,
+						success: function() {
+							setTimeout(function() {
+								uni.redirectTo({
+									url: "/pages/login/login?askUrl=" + _this.curPage
+								});
+							}, 2000);
+						}
+					}); //如果未登录则跳转到登陆页面
+				} else {
+					uni.showToast({
+						title: result.msg,
+						icon: "none",
+						duration: 2000
+					});
+				}
+			},
+			//跳转
+			tolink(Url) {
+				uni.navigateTo({
+					url: Url
+				})
+			},
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.uni-media-list-logo{ width: 100upx; height: 100upx;}
+	.uni-media-list-body{ padding: 8upx 0;}
+	.uni-media-list-r{ text-align: right; height: 84upx; padding: 8upx 0;}
+	.uni-media-list-r .time{ color: #999; line-height: 36upx;}
+	.uni-badge-red{ background: #fa3d34;}
+	.uni-list-cell::after{left: 150upx;}
+</style>
