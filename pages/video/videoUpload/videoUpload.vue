c26837df2eb0e5f0cb087d166cf7446c3f6364f2
liuze <1573335091@qq.com>
2020-07-03 16:34:30 +0800

    完成上传文件加载

--- pages/video/videoUpload/videoUpload.vue
+++ pages/video/videoUpload/videoUpload.vue
@@ -74,10 +74,16 @@
 				});
 				if(res.code==0){
 					uni.showToast({
-						title:"发布成功"
+						title:"发布成功",
 					})
 					uni.setStorageSync("fileName","");//清空缓存
 					uni.setStorageSync("filePath","")
+					setTimeout(()=>{
+						uni.navigateBack({
+						    delta: 1
+						});
+					},1000)
+					
 				}else{
 					uni.showToast({
 						title:res.msg,
