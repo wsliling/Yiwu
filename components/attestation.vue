2549c3575e6d65475e3f132c63c448af3dc11088
liuze <1573335091@qq.com>
2020-07-04 11:39:53 +0800

    认证的状态做为组件传，完成认证的4种状态

---
+++ components/attestation.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="">
+		<view class="succeed" v-if="AuditStatus == 0 || AuditStatus == 1">
+			<image src="../static/my/succeed.png" mode=""></image>
+			<view class="" v-if="AuditStatus == 0"> 认证成功</view>
+			<view class="" v-else> 待审核中</view>
+		</view>
+		<view class="succeed" v-if="AuditStatus == 2">
+			<image src="../static/my/defeated.png" mode=""></image>
+			<view class="">认证失败！</view>
+			<view class="anew" @click="anew">重新认证</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			AuditStatus: {
+				type: [String,Number],
+				default:''
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			anew(){
+				this.$emit('toAnew');
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.succeed{
+		font-size: 36upx;
+		font-weight:bold;
+		text-align: center;
+		padding-top: 100upx;
+		image{
+			width:176upx;
+			height:176upx;
+		}
+		.anew{
+			width:516upx;
+			height:88upx;
+			line-height: 88upx;
+			border:1upx solid rgba(239,53,71,1);
+			border-radius:44upx;
+			font-size:32upx;
+			color:rgba(239,53,71,1);
+			margin: 0 auto;
+			margin-top: 200upx;
+		}
+	}
+</style>
