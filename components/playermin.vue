46db090fd1f146a1e030b0d6d6783bbe458b2cc2
liling <907247899@qq.com>
2020-07-29 16:41:50 +0800

    00

---
+++ components/playermin.vue
@@ -0,0 +1,113 @@
+<template>
+		<view class="player-min" :class="[!isOpen?'open':'']" v-if="(isplayactive && require)">
+			<image class="img" :class="[isplayingmusic ? '':'stoped']" :src="playdetail.pic" mode="" @click="isOpen=!isOpen"></image>
+			<view class="btns" v-show="isOpen">
+				<text :class="['item iconfont',isplayingmusic?'icon-audiopuse':'icon-audioplay']" @click="play"></text>
+				<text class="item iconfont icon-return" @click="toPlayPage"></text>
+			</view>
+		</view>
+</template>
+
+<script>
+	import {
+		mapGetters,
+		mapMutations
+	} from 'vuex'
+	export default {
+		props:{
+			require:{
+				type:Boolean,
+				default:true
+			},
+			pagetype:{
+				type:String,
+				default:""
+			}
+		},
+		mounted() {
+			console.log(this.require)
+		},
+		data() {
+			return {
+				isOpen:false
+			};
+		},
+		computed:{
+			...mapGetters(['isplayingmusic','playdetail','audiolist','isplayactive'])
+		},
+		methods:{
+			...mapMutations(['setIsplayingmusic','setPlaydetail']),
+			play(){
+				if(this.isplayingmusic){
+					this.$au_player.pause();
+					this.setIsplayingmusic(false)
+				}else{
+					this.$au_player.play()
+					this.setIsplayingmusic(true)
+				}
+			},
+			toPlayPage(){
+				if(this.pagetype=="share"){
+					uni.navigateTo({
+						url:'/pages/music/playMusic/playMusic?type=share&id='+this.playdetail.id
+					})
+				}else{
+					const list  = this.audiolist;
+					uni.setStorageSync("musicList",list)
+					uni.navigateTo({
+						url:'/pages/music/playMusic/playMusic?id='+this.playdetail.id
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+		.player-min{
+			position: fixed;
+			bottom: 40%;
+			left: 12upx;
+			z-index: 1002;
+			display: flex;
+			align-items: center;
+			background-color: rgba(0,0,0,0.7);
+			border-radius: 50upx 15upx 15upx 50upx ;
+			&.open{
+				border-radius: 45upx ;
+			}
+			.img{
+				width: 100upx;
+				height: 100upx;
+				border-radius: 50%;
+				&.stoped {
+					animation-play-state: paused;
+				}
+				animation: rotate 10s linear .1s infinite;
+				box-shadow: 0 0 20upx;
+			}
+			.btns{
+				display: flex;
+				height: 100upx;
+				line-height: 100upx;
+				font-size: 48upx;
+				color: #F0F0F0;
+				transition: width 2.5s linear;
+				align-items: center;
+				padding: 0 10upx;
+				.item{
+					margin: 0 14upx;
+				}
+			}
+		}
+		@keyframes rotate {
+			0% {
+				transform: rotate(0deg);
+			}
+		
+			100% {
+				transform: rotate(360deg);
+			}
+		
+		}
+</style>
